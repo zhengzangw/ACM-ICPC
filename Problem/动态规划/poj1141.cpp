41cdec1009cf281c8e1e34e9f065284d08c01409
Fermat <zhengzangw@gmail.com>
2018-01-25 23:08:00 +0800

    temp

---
+++ Problem/动态规划/poj1141.cpp
@@ -0,0 +1,25 @@
+// .cpp
+/*******************************************************************  
+ *	@Problem_ID:  
+ *	@Solver: Zhengzangw
+ * 	@Date: 24/01/2018, 22:25
+ *	@Last Modified time: 24/01/2018, 22:25
+ *	考查点：
+ *	思路：
+ *	提交情况：
+ *	收获：
+ *	经验：
+ ********************************************************************/
+
+// AC Code
+#include <iostream>
+#include <cstdio>
+#include <string>
+#include <cstring>
+using namespace std;
+
+int main()
+{
+    
+    return 0;
+}
